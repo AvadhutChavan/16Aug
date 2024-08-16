ceaabb0aaa78d4568c2efc23f2f102bd4539b17f
Avadhut Test <avadhutchavan9999@gmail.com>
2024-08-16 11:16:55 +0530

    math.js

---
+++ math.js
@@ -0,0 +1,24 @@
+// mathis function used for arithmetic operations
+
+//constants value math function
+pi=Math.PI;
+console.log(pi);
+
+e=Math.E;
+console.log(e);
+
+
+// another math function 
+
+console.log(`abs methods returns absolute value=${Math.abs(-5.0)}`);
+
+console.log(`pow method returns power of two number =${Math.pow(2,4)}`);
+//2*2*2*2*=16
+
+console.log(`ceil method returns large rounde up number=${Math.ceil(-5.5)}`)
+
+console.log(`floor method returns small round up number= ${Math.floor(3.25)}`);
+
+// generate random number using Math.randon() method
+
+console.log(`random number betwen 1 to 100 is = ${Math.floor(Math.random()*100)+1}`);
